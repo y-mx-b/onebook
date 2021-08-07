d9312b12d13178e21c25b0af2b6f9853d0d59af6
luardemin <luardemin@gmail.com>
2021-08-07 23:23:30 +0900

    Added TOMLDecoder, created config.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
+        .package(url: "https://github.com/dduan/TOMLDecoder", from: "0.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -17,6 +18,7 @@ let package = Package(
             name: "onebook",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "TOMLDecoder", package: "TOMLDecoder")
             ]),
         .testTarget(
             name: "onebookTests",
