0dcc3558ba4cc7585ed8a26eb5e1cb082066a11c
luardemin <luardemin@gmail.com>
2021-08-24 10:57:12 +0900

    TODO: added new todos

--- Sources/onebook/back_end/bookmark_manager/bookmark_manager.swift
+++ Sources/onebook/back_end/bookmark_manager/bookmark_manager.swift
@@ -6,20 +6,24 @@ struct BookmarkManager {
     var storageDirectory = "\(NSHomeDirectory())/.bookmarks/"
 
     func create(_ bookmarkPath: String?, siteURL: URL?) {
-    // TODO: add bookmarks
+        // TODO: add bookmarks
+        // - create intermediate folders (if neccessary)
+        // - create bookmark file
+        // - create new GUID
+        // - insert necessary information into bookmark file
     }
 
     func remove() {
-    // TODO: remove bookmarks
+        // TODO: remove bookmarks
         // remove files
         // if folder is empty, ask to remove
     }
 
     func clean() {
-    // TODO: remove empty bookmark folders
+        // TODO: remove empty bookmark folders
     }
 
     func sync() {
-    // TODO: sync bookmarks between browsers (this might take a while)
+        // TODO: sync bookmarks between browsers (this might take a while)
     }
 }
