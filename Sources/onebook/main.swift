ca76e95e593c0806ca8b32c2d07f3c50c1b7f315
luardemin <luardemin@gmail.com>
2021-10-14 00:06:06 +0900

    Package updates Testing BookmarkParser library

--- Sources/onebook/main.swift
+++ Sources/onebook/main.swift
@@ -1,3 +1,13 @@
 import Foundation
+import BookmarkParser
 
-Onebook.main()
+let bookmark = OnebookChildren(name: "Bookmark", url: "URL", children: nil)
+let folder = OnebookChildren(name: "Folder", url: nil, children: [bookmark])
+
+let wrapper = OnebookChildrenWrapper(folder)
+let jsonData = try! JSONEncoder().encode(wrapper)
+let jsonString = String(data: jsonData, encoding: .utf8)!
+print(jsonString)
+
+
+// Onebook.main()
