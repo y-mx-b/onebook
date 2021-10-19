6adcc10003d2d3ea74f8838ce09106e5af66f3bd
luardemin <luardemin@gmail.com>
2021-10-19 20:41:26 +0900

    Removed random code in main.swift Removed *.org and document conversion script

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A universal bookmark manager (heavy WIP).
 
 Current state:
 
--   Being rewritten entirely
+ - Being rewritten entirely
--- Sources/onebook/main.swift
+++ Sources/onebook/main.swift
@@ -1,13 +1,3 @@
 import Foundation
-import BookmarkParser
 
-let bookmark = OnebookChildren(name: "Bookmark", url: "URL", children: nil)
-let folder = OnebookChildren(name: "Folder", url: nil, children: [bookmark])
-
-let wrapper = OnebookChildrenWrapper(folder)
-let jsonData = try! JSONEncoder().encode(wrapper)
-let jsonString = String(data: jsonData, encoding: .utf8)!
-print(jsonString)
-
-
-// Onebook.main()
+Onebook.main()
