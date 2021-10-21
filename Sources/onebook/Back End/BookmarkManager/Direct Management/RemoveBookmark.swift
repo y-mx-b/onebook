3125df96e6d17781bff6acec8c4c139e0f50a55f
luardemin <luardemin@gmail.com>
2021-10-21 10:47:27 +0900

    Implemented bookmark deletion

--- Sources/onebook/Back End/BookmarkManager/Direct Management/RemoveBookmark.swift
+++ Sources/onebook/Back End/BookmarkManager/Direct Management/RemoveBookmark.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 extension BookmarkManager {
-    func removeBookmark(_bookmarkPath: String) -> Bool {
+    func removeBookmark(_ bookmarkPath: String) throws -> Bool {
+        let fm = FileManager.default
+        try fm.removeItem(atPath: bookmarkPath)
         return false
     }
 }
