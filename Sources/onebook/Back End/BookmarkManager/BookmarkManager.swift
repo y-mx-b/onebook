78eeb6b96a7b7d23af5e3804d88a5570b6a465a1
luardemin <luardemin@gmail.com>
2021-11-04 10:44:36 +0900

    Added new initializers for BookmarkManager

--- Sources/onebook/Back End/BookmarkManager/BookmarkManager.swift
+++ Sources/onebook/Back End/BookmarkManager/BookmarkManager.swift
@@ -4,5 +4,20 @@ import BookmarkParser
 struct BookmarkManager {
     var browser: Browser?
     var bookmarkFilePath: String?
-    var storageDirectory: String?
+    var storageDirectory: String? = try! ConfigManager().loadPreferences().storageDirectory
+
+    init() {
+        browser = nil
+        bookmarkFilePath = nil
+    }
+
+    init(browser: Browser) {
+        self.browser = browser
+        bookmarkFilePath = bookmarkPaths[browser]
+    }
+
+    init(bookmarkFilePath: String) {
+        browser = nil
+        self.bookmarkFilePath = bookmarkFilePath
+    }
 }
